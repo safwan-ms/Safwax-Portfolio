f1895859712b2381903af42c7402a47c611d87a5
safwan-ms <safwan.anasms@gmail.com>
2025-04-30 16:50:35 +0530

    feat(service): Complete Services Section with Responsive Classes

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import About from "../About/About";
+import Services from "../Services/Services";
 import Hero from "./Hero/Hero";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div className="overflow-hidden">
       <Hero />
       <About />
+      <Services />
     </div>
   );
 };
---
+++ components/Services/ServiceCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+import Tilt from "react-parallax-tilt";
+interface Service {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+}
+interface ServiceProps {
+  service: Service;
+}
+const ServiceCard = ({ service }: ServiceProps) => {
+  return (
+    <Tilt className="shadow-2xl p-6 rounded-lg bg-gradient-to-br from-info-content to-neutral">
+      <Image src={service.icon} alt={service.title} width={50} height={50} />
+      <h1 className="mt-4 text-lg font-bold text-white">{service.title}</h1>
+      <p className="mt-3 text-sm text-gray-300">{service.description}</p>
+    </Tilt>
+  );
+};
+
+export default ServiceCard;
